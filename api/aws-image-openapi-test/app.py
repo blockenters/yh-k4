b296f25eeeaa91eb1b815b13ac6129a85f1a7d39
blockenters <blockenters@gmail.com>
2023-06-26 17:01:23 +0900

    add

--- api/aws-image-openapi-test/app.py
+++ api/aws-image-openapi-test/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.photo import PhotoResource, PhotoReviewResource
+from resources.search import NewsSearchResource
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ api = Api(app)
 
 api.add_resource( PhotoResource ,  '/photo')
 api.add_resource( PhotoReviewResource  , '/review')
+api.add_resource( NewsSearchResource  , '/search/news')
 
 if __name__ == '__main__':
     app.run()
